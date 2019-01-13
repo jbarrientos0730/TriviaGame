2c347face09842d6c26ebd2e1933a5bf1d2383c8
Jorge Barrientos <jorgebarrientos0311@gmail.com>
2019-01-12 23:07:18 -0600

    Final

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,9 +22,57 @@ $(document).ready(function() {
         hide();
         triviaSumm();
         stop();
-    }):
+    });
 
     function triviaSumm(){
-        
+        var alldone = $('<h2>').html('All Done!');
+        var canswers = $('<h4>').html('Correct answers: ' + correct);
+        var ianswers = $('<h4>').html('Incorrect answers: ' + incorrect);
+        var qunanswered = $('<h4>').html('Unanswered: ' + unanswered);
+        var newclass= $('<div class="col-lg-4 col-lg-offset-4 text-center" id="summary">');
+        newclass.append(alldone);
+        newclass.append(canswers);
+        newclass.append(ianswers);
+        newclass.append(qunanswered);
+        $(".summ").append(newclass);
     }
-}
+
+    function decrement() {
+    time--;
+
+    $("#timer").html(" " + time + " seconds");
+    
+    if (time > 1) {
+        $("#timer").html(" " + time + " second");
+        }
+        else if (time === 0) {
+            $('#triviaStart').hide();
+            hide();
+            triviaSumm();
+            stop();
+        }
+   }
+
+    function stop() {
+        clearInterval(intervalId);
+    }  
+
+    function hide(){
+        $(".questions").hide();
+        $("#triviaStart").hide();
+        $("#finish").hide();
+    }
+
+    function show() {
+        $(".questions").show();
+        $("#triviaStart").show();
+        $("#finish").show();
+    }
+
+    $('input[type=radio]').on("change", function() {
+        correct =  $('input[value=correct]:checked').length;
+        incorrect = $('input[value=wrong]:checked').length;
+        unanswered = (4-(correct + incorrect));
+    });
+
+});
